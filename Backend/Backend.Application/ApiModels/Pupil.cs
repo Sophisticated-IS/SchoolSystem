31f41760897a013418e1df46cc72e0a18466fd0c
Igor SI <ihhjk@ya.ru>
2023-04-08 22:36:17 +0500

    добавил модели для апи

---
+++ Backend/Backend.Application/ApiModels/Pupil.cs
@@ -0,0 +1,11 @@
+﻿using Backend.Application.Common.Mappings;
+
+namespace Backend.Application.ApiModels;
+
+public sealed class Pupil : IMapWith<Domain.Pupil>
+{
+    public uint Id { get; init; }
+    public string Name { get; set; }
+    public string SurName { get; set; }
+    public string MiddleName { get; set; }
+}
---
+++ Backend/Backend.Application/ApiModels/Teacher.cs
@@ -0,0 +1,12 @@
+﻿using Backend.Application.Common.Mappings;
+
+namespace Backend.Application.ApiModels;
+
+public sealed class Teacher : IMapWith<Domain.Teacher>
+{
+    public uint Id { get; init; }
+    public string Name { get; set; }
+    public string SurName { get; set; }
+    public string MiddleName { get; set; }
+    public string Comment { get; set; }
+}
