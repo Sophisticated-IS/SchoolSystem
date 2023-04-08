4bf861b10d5fb13b0367be45214993f3c152feda
Igor SI <ihhjk@ya.ru>
2023-04-08 22:35:25 +0500

    добавил команду создания учителя

---
+++ Backend/Backend.Application/Commands/CreateTeacherCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Backend.Application.Commands;
+
+public sealed class CreateTeacherCommand : IRequest<ApiModels.Teacher>
+{
+    public string Name { get; set; }
+    public string SurName { get; set; }
+    public string MiddleName { get; set; }
+    public string Comment { get; set; }
+}
---
+++ Backend/Backend.Application/Commands/Handlers/CreateTeacherCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Backend.Domain;
+using MediatR;
+
+namespace Backend.Application.Commands.Handlers;
+
+internal sealed class CreateTeacherCommandHandler : IRequestHandler<CreateTeacherCommand,ApiModels.Teacher>
+{
+    
+    private readonly ISchoolDbContext _schoolDbContext;
+    private readonly IMapper _mapper;
+
+    public CreateTeacherCommandHandler(ISchoolDbContext schoolDbContext, IMapper mapper)
+    {
+        _schoolDbContext = schoolDbContext;
+        _mapper = mapper;
+    }
+    public async Task<ApiModels.Teacher> Handle(CreateTeacherCommand request, CancellationToken cancellationToken)
+    { 
+        var teacher = new Teacher()
+        {
+            Name = request.Name,
+            SurName = request.SurName,
+            MiddleName = request.MiddleName,
+            Comment = request.Comment
+        };
+        
+        await _schoolDbContext.Teachers.AddAsync(teacher, cancellationToken).ConfigureAwait(false);
+        await _schoolDbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+       
+        return _mapper.Map<ApiModels.Teacher>(teacher);
+    }
+}
