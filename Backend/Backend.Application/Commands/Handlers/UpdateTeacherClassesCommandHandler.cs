b7370b0baa0cfc25099ef3484feb3ee4d1ae4891
Igor SI <ihhjk@ya.ru>
2023-04-09 19:08:39 +0500

    добавил обновление классов в которых преподают учителя

---
+++ Backend/Backend.Application/Commands/Handlers/UpdateTeacherClassesCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Application.Commands.Handlers;
+
+public sealed class UpdateTeacherClassesCommandHandler : IRequestHandler<UpdateTeacherClassesCommand,uint[]>
+{
+    private readonly ISchoolDbContext _schoolDbContext;
+        
+    public UpdateTeacherClassesCommandHandler(ISchoolDbContext schoolDbContext)
+    {
+        _schoolDbContext = schoolDbContext;
+    }
+
+    public async Task<uint[]> Handle(UpdateTeacherClassesCommand request, CancellationToken cancellationToken)
+    {
+        var teacher = await _schoolDbContext.Teachers.FirstOrDefaultAsync((t) => t.Id == request.TeacherId, cancellationToken);
+        if (teacher is null) return Array.Empty<uint>();
+
+        var hashIds = request.ClassIds.ToHashSet();
+        var classes = _schoolDbContext.Classes.Where(c => hashIds.Contains(c.Id));
+
+        teacher.Classes = classes.ToList();
+
+        await _schoolDbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        
+        return classes.Select(c => c.Id).ToArray();
+    }
+}
---
+++ Backend/Backend.Application/Commands/UpdateTeacherClassesCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Backend.Application.Commands;
+
+public sealed class UpdateTeacherClassesCommand : IRequest<uint[]>
+{
+    public uint[] ClassIds { get; init; }
+    public uint TeacherId { get; init; }
+
+    public UpdateTeacherClassesCommand(uint teacherId, uint[] classIds)
+    {
+        ClassIds = classIds;
+        TeacherId = teacherId;
+    }
+}
