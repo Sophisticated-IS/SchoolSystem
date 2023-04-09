9fd2617238b4b051c4eb8ed32b1973da55c81aa4
Igor SI <ihhjk@ya.ru>
2023-04-09 19:09:45 +0500

    добавил query для получения учителей в классе

---
+++ Backend/Backend.Application/Queries/GetClassTeacherQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Backend.Application.Queries;
+
+public sealed class GetClassTeacherQuery : IRequest<uint[]>
+{
+    public uint ClassId { get; init; }
+
+    public GetClassTeacherQuery(uint classId )
+    {
+        ClassId = classId;
+    }
+}
---
+++ Backend/Backend.Application/Queries/Handlers/GetClassTeacherHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Application.Queries.Handlers;
+
+public sealed class GetClassTeacherHandler : IRequestHandler<GetClassTeacherQuery, uint[]>
+{
+    private readonly ISchoolDbContext _schoolDbContext;
+
+
+    public GetClassTeacherHandler(ISchoolDbContext schoolDbContext)
+    {
+        _schoolDbContext = schoolDbContext;
+    }
+    
+    public async Task<uint[]> Handle(GetClassTeacherQuery request, CancellationToken cancellationToken)
+    {
+        var classTeacher = await _schoolDbContext.Classes
+                                                 .Include(p=>p.Teachers)
+                                                 .FirstOrDefaultAsync(ct => ct.Id == request.ClassId, cancellationToken: cancellationToken);
+        if (classTeacher is null) return Array.Empty<uint>();
+        
+        
+        return classTeacher.Teachers.Select(t=>t.Id).ToArray();
+        
+    }
+}
