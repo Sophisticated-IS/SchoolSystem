2a0a300bfc50472b349c15df040f1d7b25b1ed16
Igor SI <ihhjk@ya.ru>
2023-04-09 19:10:40 +0500

    добавил query получения учителя по id

---
+++ Backend/Backend.Application/Queries/GetTeacherByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using Backend.Application.ApiModels;
+using MediatR;
+
+namespace Backend.Application.Queries;
+
+public sealed class GetTeacherByIdQuery : IRequest<TeacherWithId>
+{
+    public uint Id { get; init; }
+
+    public GetTeacherByIdQuery(uint id)
+    {
+        Id = id;
+    }
+
+}
---
+++ Backend/Backend.Application/Queries/Handlers/GetTeacherByIdHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Backend.Application.ApiModels;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Application.Queries.Handlers;
+
+public sealed class GetTeacherByIdHandler : IRequestHandler<GetTeacherByIdQuery, TeacherWithId>
+{
+    private readonly ISchoolDbContext _schoolDbContext;
+    private readonly IMapper _mapper;
+
+    public GetTeacherByIdHandler(ISchoolDbContext schoolDbContext, IMapper mapper)
+    {
+        _schoolDbContext = schoolDbContext;
+        _mapper = mapper;
+    }
+
+    public async Task<TeacherWithId> Handle(GetTeacherByIdQuery request, CancellationToken cancellationToken)
+    {
+        var teacher = await _schoolDbContext.Teachers.FirstOrDefaultAsync(t => t.Id == request.Id, cancellationToken: cancellationToken)
+                                            .ConfigureAwait(false);
+        if (teacher is null) throw new Exception("Teacher not found");
+        
+        
+        return _mapper.Map<TeacherWithId>(teacher);
+    }
+}
