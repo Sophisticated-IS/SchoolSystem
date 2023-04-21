29030e82056d8d843f6e270d16eaaade877bbb10
Igor SI <ihhjk@ya.ru>
2023-04-21 21:26:38 +0500

    удалил лишний проект

--- НеверовАнализУязвимостейПО/Models/DataBase/Schema/ParallelLevel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace НеверовАнализУязвимостейПО.Models.DataBase.Schema;
-
-public sealed class ParallelLevel
-{
-    [Key] public int Id { get; init; }
-    public string Level { get; set; }
-}
