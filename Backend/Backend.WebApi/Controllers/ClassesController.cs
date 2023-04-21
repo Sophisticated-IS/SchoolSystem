78d1a882f5d674017c6774edffa76e42dd5830ae
Igor SI <ihhjk@ya.ru>
2023-04-21 19:36:45 +0500

    добавил валидацию ClassesController

--- Backend/Backend.WebApi/Controllers/ClassesController.cs
+++ Backend/Backend.WebApi/Controllers/ClassesController.cs
@@ -30,6 +30,13 @@ public class ClassesController : Controller
     [HttpGet("{id}")]
     public async Task<IActionResult> GetClassById(uint id)
     {
+        if (id == 0)
+        {
+            ModelState.AddModelError(nameof(id), $"{nameof(id)} cannot be 0");
+            return BadRequest(ModelState);
+        }
+        
+        
         var result = await _mediator.Send(new GetClassByIdQuery(id));
         return Ok(result);
     }
@@ -38,6 +45,12 @@ public class ClassesController : Controller
     [HttpGet("TeachersInClass")]
     public async Task<IActionResult> GetClassTeachers(uint classId)
     {
+        if (classId == 0)
+        {
+            ModelState.AddModelError(nameof(classId), $"{nameof(classId)} cannot be 0");
+            return BadRequest(ModelState);
+        }
+        
         var result = await _mediator.Send(new GetClassTeacherQuery(classId));
         return Ok(result);
     }
